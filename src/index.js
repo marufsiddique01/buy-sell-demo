3aa53631975280eb5ffa46c0060a5c0df1d51276
marufsiddique01 <maruf.siddique@northsouth.edu>
2022-12-14 19:50:08 +0600

    mongodb connected

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 // import packages
 const { response } = require('express')
 const express = require('express')
+const dotenv = require('dotenv').config()
 
 // import routes and assets
-
 const quotes = require('./quotes.json')
 const userRouter = require('./routes/userRoutes')
 const noteRouter = require('./routes/noteRoutes')
 
 // start app
 const app = express()
+
+// dotenv variables
+const port = process.env.PORT || 5000
+const mongodbURL = process.env.MONGODB
+
+// import mongoose
+const mongoose = require('mongoose')
+
+// connect mongodb; if connected start the app
+mongoose
+  .connect(mongodbURL)
+  .then(() => {
+    console.log('Connected to database')
+    app.listen(port, () => {
+      console.log('server started on the port: 5000')
+    })
+  })
+  .catch((e) => {
+    console.log(e)
+  })
 
 app.use('/users', userRouter)
 app.use('/notes', noteRouter)
@@ -26,8 +46,4 @@ app.get('/random', (req, res) => {
   let index = Math.floor(Math.random() * quotes.length)
   let quote = quotes[index]
   res.status(200).json(quote)
-})
-
-app.listen(5000, () => {
-  console.log('server started on the port: 5000')
 })
