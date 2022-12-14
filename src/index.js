d1a17184969c3f77dd7c454dd3392efbdbd3ab1f
marufsiddique01 <maruf.siddique@northsouth.edu>
2022-12-14 18:36:01 +0600

    local api done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Hello World')
+// import packages
+const { response } = require('express')
+const express = require('express')
+
+const quotes = require('./quotes.json')
+
+// start app
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+app.get('/quotes', (req, res) => {
+  // res.send('Quote')
+  res.status(200).json(quotes)
+})
+
+app.get('/random', (req, res) => {
+  let index = Math.floor(Math.random() * quotes.length)
+  let quote = quotes[index]
+  res.status(200).json(quote)
+})
+
+app.listen(5000, () => {
+  console.log('server started on the port: 5000')
+})
