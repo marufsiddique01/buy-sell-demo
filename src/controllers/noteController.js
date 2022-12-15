611e8d98551f2bcfa312376e53bbd3b519093947
marufsiddique01 <maruf.siddique@northsouth.edu>
2022-12-15 13:31:06 +0600

    auth middleware in notes

---
+++ src/controllers/noteController.js
@@ -0,0 +1,13 @@
+const noteModel = require('../model/note')
+
+const createNote = (req, res) => {}
+const updateNote = (req, res) => {}
+const deleteNote = (req, res) => {}
+const getNotes = (req, res) => {}
+
+module.exports = {
+  createNote,
+  updateNote,
+  deleteNote,
+  getNotes,
+}
