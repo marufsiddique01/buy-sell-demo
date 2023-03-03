c8a69ed476f160c3fa588b848133d334bf6f8179
marufsiddique01 <siddiquemaruf7@gmail.com>
2023-03-03 12:35:58 +0600

    hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Header from '@/components/layout/Header'
-import Image from 'next/image'
+import Hero from '@/components/layout/Hero'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
+
     </>
   )
 }
